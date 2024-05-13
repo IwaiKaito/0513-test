f6ade2300424f601cc5a4b901f7b6f95b15684bc
IwaiKaito <231205027@ccmailg.meijo-u.ac.jp>
2024-05-13 15:57:24 +0900

    ピースの色を変更

--- js/tetris.js
+++ js/tetris.js
@@ -22,7 +22,7 @@ var shapes = [
       1, 1, 1 ]
 ];
 var colors = [
-    'cyan', 'orange', 'blue', 'yellow', 'red', 'red', 'purple'
+    'cyan', 'orange', 'skyblue', 'yellow', 'red', 'red', 'purple'
 ];
 
 // creates a new 4x4 shape in global variable 'current'
